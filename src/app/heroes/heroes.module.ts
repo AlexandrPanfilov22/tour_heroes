3d3598df607529295b276c735cff4a4786f3e23e
Александр Панфилов <panfilove4516@gmail.com>
2023-04-04 15:00:30 +0300

    feat (hero-detail): Create hero-detail component

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     CommonModule,
